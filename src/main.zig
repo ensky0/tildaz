dddb085b255d8e340fc9ab2f26dfb68978f9e7fe
ensky0 <ensky0@gmail.com>
2026-04-24 08:27:01 +0900

    플랫폼별 host 선택 경계 추가
    main.zig에서 OS별 실행 host를 선택하고 미지원 플랫폼용 unsupported host를 추가한다.

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,16 @@
 const std = @import("std");
-const windows_host = @import("windows_host.zig");
+const builtin = @import("builtin");
+const host = switch (builtin.os.tag) {
+    .windows => @import("windows_host.zig"),
+    else => @import("unsupported_host.zig"),
+};
 
 /// ReleaseFast에서도 crash 원인을 표시하는 panic handler
 pub fn panic(msg: []const u8, _: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
     const addr = ret_addr orelse @returnAddress();
-    windows_host.showPanic(msg, addr);
+    host.showPanic(msg, addr);
 }
 
 pub fn main() void {
-    windows_host.run() catch |err| windows_host.showFatalRunError(err);
+    host.run() catch |err| host.showFatalRunError(err);
 }
